d37f80d99e062e914de8dfae5a6d661db9fb94cf
Chiaraggi21 <cguariglia22@fed.idserve.net>
2022-11-24 17:22:43 +0100

    gg

---
+++ BeerWithMe/BeerWithMe/BeerWithMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeerWithMeApp.swift
+//  BeerWithMe
+//
+//  Created by Chiara Guariglia on 24/11/22.
+//
+
+import SwiftUI
+
+@main
+struct BeerWithMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
